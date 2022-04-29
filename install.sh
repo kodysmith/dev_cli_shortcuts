08165b72da9c9a4670575849d5ffa72c388f9522
Kody Smith <ksmith@coursera.org>
2022-04-28 17:14:46 -0700

    cleaning up and adding comments to the install script

--- install.sh
+++ install.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
-echo "what file is sourced at startup? example: .profile"
-read PROFILE;
-echo "installing scripts in $PROFILE."
+
+# constants
 CWD=$(PWD);
 LIVE=$CWD/live/;
 cp $CWD/generic/* $CWD/live/;
+
+
+echo "what file is sourced at startup? example: .profile or .bashrc or .bash_profile"
+read PROFILE;
+echo "installing scripts in $PROFILE."
+
+
+# create the base directory to go live into;
+mkdir live;
+
+# write the bootstrapping script to the .bashrc or .profile file
 echo "\
 ##### dev_cli_tools start #####
 LIVE=$LIVE;
 "'
-for f in $(ls $LIVE);
-do;
+for f in $(ls $LIVE)
+do
   source $LIVE/$f;
-done;
+done
 ##### dev_cli_tools end #######
 ' >> ~/$PROFILE;
+
+
+
+# source the scripts right away to see it work immediately
 source ~/$PROFILE;
